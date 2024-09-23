5602030f66a4b947f97899ea019d407a79eabd7e
dffesalbon <fesalbondaniel@gmail.com>
2024-09-23 10:31:51 +0800

    update: checkpoint path

--- sam_utils.py
+++ sam_utils.py
@@ -12,7 +12,7 @@ DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 MODEL_TYPE = "vit_h"
 HOME = os.getcwd()
 print("HOME:", HOME)
-CHECKPOINT_PATH = os.path.join(HOME, "weights", "sam_vit_h_4b8939.pth") ## sam weights location
+CHECKPOINT_PATH = os.path.join(HOME, "weights", "sam_b.pt") ## sam weights location
 
 def scale_image(image_bytes):
   size_scale = 800
